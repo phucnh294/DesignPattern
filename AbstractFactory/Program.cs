4ed1d794f9acb8dd36c5fc1ebf8f04d11d77a511
Phuc Nguyen <phucnh294@gmail.com>
2019-03-13 12:35:50 -0500

    first commit

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create and run asian world
+            var asian = new AnimalWorld<Asian>();
+            asian.FoodChain();
+            //create and run america world
+            var america = new AnimalWorld<America>();
+            america.FoodChain();
+
+            Console.ReadKey();
+        }
+    }
+
+    interface IContinentFactory
+    {
+        IHerbivore CreateHerbivore();
+        ICanivore CreateCanivore();
+    }
+
+    interface IHerbivore
+    {
+        void Eat();
+    }
+
+    interface ICanivore
+    {
+        void Eat(IHerbivore herbivore);
+    }
+
+    class Asian : IContinentFactory
+    {
+        public IHerbivore CreateHerbivore()
+        {
+            return new Donkey();
+        }
+
+        public ICanivore CreateCanivore()
+        {
+            return new Tiger();
+        }
+    }
+
+    class America : IContinentFactory
+    {
+        public IHerbivore CreateHerbivore()
+        {
+            return new Bisson();
+        }
+
+        public ICanivore CreateCanivore()
+        {
+            return new Lion();
+        }
+    }
+
+
+    class Tiger : ICanivore
+    {
+        public void Eat(IHerbivore herbivore)
+        {
+            Console.WriteLine(this.GetType().Name + " eats " + herbivore.GetType().Name);
+        }
+    }
+
+    class Donkey : IHerbivore
+    {
+        public void Eat()
+        {
+            Console.WriteLine(this.GetType().Name + "eats grass");
+        }
+    }
+
+    class Lion : ICanivore
+    {
+        public void Eat(IHerbivore herbivore)
+        {
+            Console.WriteLine(this.GetType().Name + " eats " + herbivore.GetType().Name);
+        }
+    }
+
+    class Bisson : IHerbivore
+    {
+        public void Eat()
+        {
+            Console.WriteLine(this.GetType().Name + " eats leaves");
+        }
+    }
+
+    interface IAnimalWorld
+    {
+        void FoodChain();
+    }
+
+    class AnimalWorld<T> : IAnimalWorld where T : IContinentFactory, new()
+    {
+        private IHerbivore herbivore;
+        private ICanivore canivore;
+        private T factory;
+
+        public AnimalWorld()
+        {
+            factory = new T();
+            herbivore = factory.CreateHerbivore();
+            canivore = factory.CreateCanivore();
+        } 
+
+        public void FoodChain()
+        {
+            herbivore.Eat();
+            canivore.Eat(herbivore);
+        }
+    }
+
+
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.Insctance().valueOne = 10;
+            Calculator.Insctance().valueTwo = 5;
+            
+            Console.WriteLine("Add : " + Calculator.Insctance().Add());
+            Console.WriteLine("Sub : " + Calculator.Insctance().Subtract());
+            Console.WriteLine("Multi : " + Calculator.Insctance().Multiple());
+            Console.WriteLine("Devide : " + Calculator.Insctance().Divide());
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Calculator
+    {
+       private Calculator() { }
+
+        private static readonly Calculator instance = new Calculator();
+
+        public static Calculator Insctance()
+        {
+//            if (instance == null)
+//            {
+//                instance= new Calculator();
+//            }
+            return instance;
+        }
+        public int valueOne { get; set; }
+        public int valueTwo { get; set; }
+
+        public int Add()
+        {
+            return valueOne + valueTwo;
+        }
+
+        public int Subtract()
+        {
+            return valueOne - valueTwo;
+        }
+
+        public int Multiple()
+        {
+            return valueOne*valueTwo;
+        }
+
+        public int Divide()
+        {
+            return valueOne/valueTwo;
+        }
+    }
+}
